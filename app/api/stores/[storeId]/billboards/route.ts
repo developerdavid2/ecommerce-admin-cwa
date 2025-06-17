4b48d2708ec7edf1707b975a9d648341cbc4c8b3
Jacobs David <jacobsdavid.dr@gmail.com>
2025-06-17 22:01:05 +0100

    fixed billboard api params issue

--- app/api/stores/[storeId]/billboards/route.ts
+++ app/api/stores/[storeId]/billboards/route.ts
@@ -53,7 +53,7 @@ export async function POST(
 
 export async function GET(
   req: Request,
-  { params }: { params: { storeId: string } },
+  { params }: { params: Promise<{ storeId: string }> },
 ) {
   try {
     const { storeId } = await params;

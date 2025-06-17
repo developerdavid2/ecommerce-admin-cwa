6fd2447c224ef7f89b29e9d04ba7600f12bc4870
Jacobs David <jacobsdavid.dr@gmail.com>
2025-06-17 19:17:02 +0100

    fixed billboard page props

--- app/(dashboard)/stores/[storeId]/(routes)/billboards/page.tsx
+++ app/(dashboard)/stores/[storeId]/(routes)/billboards/page.tsx
@@ -1,38 +1,30 @@
 import React from "react";
-import BillboardClient from "@/app/(dashboard)/stores/[storeId]/(routes)/billboards/components/billboard-client";
 import prismadb from "@/lib/prismadb";
-import { BillboardColumn } from "@/app/(dashboard)/stores/[storeId]/(routes)/billboards/components/column";
-import { format } from "date-fns";
+import BillboardForm from "@/app/(dashboard)/stores/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form";
 
-const BillboardsSetupPage = async ({
-  params,
-}: {
-  params: { storeId: string };
-}) => {
-  const { storeId } = await params;
+interface BillboardPageProps {
+  params: {
+    billboardId: string;
+    storeId: string;
+  };
+}
 
-  const billboards = await prismadb.billboard.findMany({
+const BillboardPage: React.FC<BillboardPageProps> = async ({ params }) => {
+  const { billboardId } = params;
+
+  const billboard = await prismadb.billboard.findUnique({
     where: {
-      storeId,
-    },
-    orderBy: {
-      createdAt: "desc",
+      id: billboardId,
     },
   });
-
-  const formattedBillboards: BillboardColumn[] = billboards.map((item) => ({
-    id: item.id,
-    label: item.label,
-    imageUrl: item.imageUrl,
-    createdAt: format(item.createdAt, "MMMM do, yyyy"),
-  }));
 
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <BillboardClient data={formattedBillboards} />
+        <BillboardForm initialData={billboard} />
       </div>
     </div>
   );
 };
-export default BillboardsSetupPage;
+
+export default BillboardPage;
